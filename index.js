72b2a428a22e41ba8e2fa29f68aa182d17cd8cca
Aidanchase <aidanmro@gmail.com>
2019-06-27 00:31:00 +0000

    Summed the nums of the nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
+  let counter = 0;
   // Sum all the numbers in the array
-}
+  array.forEach((item) => {
+    if (Array.isArray(item)){
+     counter += sumItems(item)
+    } else {
+    counter += item;
+    }
+    })
+    return counter;
+  }
+
 
 module.exports = sumItems;
